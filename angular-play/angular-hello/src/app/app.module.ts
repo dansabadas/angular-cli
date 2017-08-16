d4eb168db95534930feb66e99fe5c11289446eac
AD001\z003p8vs <dan.sabadas@gmail.com>
2017-08-16 15:35:19 +0300

    generating new routing app done

--- angular-play/angular-hello/src/app/app.module.ts
+++ angular-play/angular-hello/src/app/app.module.ts
@@ -4,17 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
 import { OrdersComponent } from './orders/orders.component';
-
+import { SearchBoxDirective } from './search-box.directive';
+import { SalesDataService } from './sales-data.service';
+import { SalesData2Service } from './sales-data2.service';
+import { InitCapsPipe } from './shared/init-caps.pipe';
+import { LoginModule } from './login/login.module';
 @NgModule({
   declarations: [
     AppComponent,
     CustomerComponent,
-    OrdersComponent
+    OrdersComponent,
+    SearchBoxDirective,
+    InitCapsPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginModule
   ],
-  providers: [],
+  providers: [SalesDataService, SalesData2Service],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
