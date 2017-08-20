a3628e9402ab254486126fa6437088715ae76b77
DESKTOP-FSNB2S3\dan_s <dan.sabadas@gmail.com>
2017-08-20 15:14:19 +0300

    arrived at e2e tests

--- angular-play/angular-routing/src/app/admin/users/users.component.ts
+++ angular-play/angular-routing/src/app/admin/users/users.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: any[];
+  hasPermission = true;
 
   constructor() { }
 
   ngOnInit() {
+    if (this.hasPermission){
+        this.getUsers()
+          .then(users => this.users = users)
+          .catch(e => console.log(e.message));
+    }else {
+        this.users = [];
+    }
   }
 
+  async getUsers() {
+    return [
+      {name: 'dan', email: 'dan.sabadis@siemens.com'},
+      {name: 'john', email: 'john@siemens.com'}
+    ];
+  }
 }
